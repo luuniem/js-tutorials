f0d2cdf60aa4e61953fc39a6a109e279b6d7e78a
Niem Nguyen <luuniem87@gmail.com>
2019-04-13 22:49:07 -0700

    add arrays

---
+++ arrays/todo.js
@@ -0,0 +1,13 @@
+const todos = [
+  "Learn JS",
+  "Learn React",
+  "Watch Game of Thrones",
+  "Get Better Job",
+  "Go on Vacation"
+];
+
+console.log(
+  `You have ${todos.length} things you need to do. They are ${todos[0]}, ${
+    todos[3]
+  }, and ${todos[todos.length - 1]}.`
+);

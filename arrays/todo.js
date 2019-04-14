ec0c0d27a18f76f665d9b25d22296fe3016b1232
Niem Nguyen <luuniem87@gmail.com>
2019-04-13 23:19:23 -0700

    worked on todo list using arrays and forEach, but I choose to use map

--- arrays/todo.js
+++ arrays/todo.js
@@ -6,8 +6,23 @@ const todos = [
   "Go on Vacation"
 ];
 
-console.log(
-  `You have ${todos.length} things you need to do. They are ${todos[0]}, ${
-    todos[3]
-  }, and ${todos[todos.length - 1]}.`
-);
+// console.log(
+//   `You have ${todos.length} things you need to do. They are ${todos[0]}, ${
+//     todos[3]
+//   }, and ${todos[todos.length - 1]}.`
+// );
+
+// todos.splice(3, 1, "This is the new 2nd item");
+
+// console.log(todos.length);
+// console.log(todos);
+
+// todos.forEach(function(item, index) {
+//   console.log(index);
+//   console.log(item);
+// });
+
+todos.map((item, index) => {
+  const num = index + 1;
+  console.log(`${num}. ${item}`);
+});

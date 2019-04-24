c0f71dfcd5969c47d462603f777839047ee59236
Niem Nguyen <luuniem87@gmail.com>
2019-04-24 12:42:59 -0700

    add check box and change function

--- notes-app/main.js
+++ notes-app/main.js
@@ -49,3 +49,7 @@ document.querySelector("#name-form").addEventListener("submit", function(e) {
   console.log(e.target.elements.firstName.value);
   e.target.elements.firstName.value = "";
 });
+
+document.querySelector("#for-fun").addEventListener("change", function(e) {
+  console.log(e.target.checked);
+});

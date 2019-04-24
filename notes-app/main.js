f723857568387d7479e34d075195201d1770f60e
Niem Nguyen <luuniem87@gmail.com>
2019-04-23 21:35:04 -0700

    add form to notes, filtered todos complete

---
+++ notes-app/main.js
@@ -0,0 +1,50 @@
+const notes = [
+  {
+    title: "My next trip",
+    body: "I would like to go to Hawaii"
+  },
+  {
+    title: "Habbits to work on",
+    body: "Stop procrastinating"
+  },
+  {
+    title: "Office modifications",
+    body: "Get a standing desk"
+  }
+];
+
+const filters = {
+  searchText: ""
+};
+
+const renderNotes = function(notes, filters) {
+  const filteredNotes = notes.filter(function(note) {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+
+  document.querySelector("#notes").innerHTML = "";
+
+  filteredNotes.forEach(function(note) {
+    const noteEl = document.createElement("p");
+    noteEl.textContent = note.title;
+    document.querySelector("#notes").appendChild(noteEl);
+  });
+};
+
+renderNotes(notes, filters);
+
+const createNoteButton = document
+  .querySelector("#create-note")
+  .addEventListener("click", function(e) {
+    e.target.textContent = "Button got clicked";
+  });
+
+document.querySelector("#search-text").addEventListener("input", function(e) {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
+
+document.querySelector("#name-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  console.log(e.target.elements.firstName.value);
+});
--- todo-app/main.js
+++ todo-app/main.js
@@ -30,32 +30,34 @@ const filters = {
   searchText: ""
 };
 
-const renderedTodos = function(todos, filters) {
-  const filteredNotes = todos.filter(function(todo) {
+const renderTodos = function(todos, filters) {
+  const filteredTodos = todos.filter(function(todo) {
     return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
   });
-  console.log(renderedTodos(todos, filters));
+  const incompleteTodos = filteredTodos.filter(function(todo) {
+    return !todo.completed;
+  });
+
+  document.querySelector("#todos").innerHTML = "";
+
+  const summary = document.createElement("h2");
+  summary.textContent = `You have ${incompleteTodos.length} todos left`;
+  document.querySelector("#todos").appendChild(summary);
+
+  filteredTodos.forEach(function(todo) {
+    const p = document.createElement("p");
+    p.textContent = todo.text;
+    document.querySelector("#todos").appendChild(p);
+  });
 };
 
-console.log(filters);
-
-const incompleteTodos = todos.filter(function(todo) {
-  return !todo.completed;
-});
-const summary = document.createElement("h3");
-summary.textContent = `You have ${incompleteTodos.length} todos left`;
-document.querySelector("body").appendChild(summary);
-
-todos.forEach(function(todo) {
-  const p = document.createElement("p");
-  p.textContent = todo.text;
-  document.querySelector("body").appendChild(p);
-});
+renderTodos(todos, filters);
 
 document.querySelector("#add-todo").addEventListener("click", function(e) {
   console.log("Add a new todo");
 });
 
-document.querySelector("#new-todo").addEventListener("input", function(e) {
-  console.log(e.target.value);
+document.querySelector("#search-text").addEventListener("input", function(e) {
+  filters.searchText = e.target.value;
+  renderTodos(todos, filters);
 });
